8f639c6f3ecdf2229592df4e6fe14b44d003cc92
jurako <jurako91@gmail.com>
2022-04-24 20:41:22 +0300

    Fixed 24.94

--- carousel.js
+++ carousel.js
@@ -1,7 +1,7 @@
 const SELECTOR_CAROUSEL     = '.carousel';
 const SELECTOR_IMAGES_WRAP  = '.images';
 const SELECTOR_BC_WRAP      = '.breadcrumbs';
-const SElECTOR_NEXT         = '.next';
+const SELECTOR_NEXT         = '.next';
 const SELECTOR_PREV         = '.prev';
 const SELECTOR_TRANSITION   = '.transition';
 const SELECTOR_ACTIVE       = '.active';
@@ -9,8 +9,8 @@ const SELECTOR_ACTIVE_BC    = '.breadcrumb.active';
 
 const CLASS_NAME_NEXT       = 'next';
 const CLASS_NAME_PREV       = 'prev';
-const CLASS_NAME_MOVE_LEFT  = 'move-left';
-const CLASS_NAME_MOVE_RIGHT = 'move-right';
+const CLASS_NAME_HIDE_LEFT  = 'hide-left';
+const CLASS_NAME_HIDE_RIGHT = 'hide-right';
 const CLASS_NAME_TRANSITION = 'transition';
 const CLASS_NAME_ACTIVE     = 'active';
 
@@ -19,12 +19,12 @@ const TIMEOUT_INTERVAL      = 3000;
 class Slider {
     constructor(){
         this.timeoutId = 0;
-        this.timeoutInterval = CLASS_NAME_ACTIVE;
+        this.timeoutInterval = TIMEOUT_INTERVAL;
         this.defaultDirection = CLASS_NAME_NEXT;
         this.images = document.querySelector(SELECTOR_IMAGES_WRAP);
 
         document.querySelector(SELECTOR_PREV).addEventListener('click', this.buttonClick.bind(this));
-        document.querySelector(SElECTOR_NEXT).addEventListener('click', this.buttonClick.bind(this));
+        document.querySelector(SELECTOR_NEXT).addEventListener('click', this.buttonClick.bind(this));
         document.querySelector(SELECTOR_BC_WRAP).addEventListener('click', this.breadcrumbClick.bind(this));
     }
 
@@ -68,31 +68,20 @@ class Slider {
     }
 
     transition(newActive, direction) {
+        //return if another animation is active
+        //TO DO: check only within current sliders boundries
         if(document.querySelectorAll(SELECTOR_TRANSITION).length > 0) return;
 
-        if(direction == CLASS_NAME_NEXT) {
+        if(![CLASS_NAME_NEXT, CLASS_NAME_PREV].includes(direction)) return;
 
-            newActive.classList.add(CLASS_NAME_MOVE_RIGHT);
-            this._reflow(newActive);
+        this.prepareNextSlide(newActive, direction);
 
-            this.active.classList.add(CLASS_NAME_TRANSITION);
-            newActive.classList.add(CLASS_NAME_TRANSITION);
+        this.active.classList.add(CLASS_NAME_TRANSITION);
+        newActive.classList.add(CLASS_NAME_TRANSITION);
 
-            this.active.classList.add(CLASS_NAME_MOVE_LEFT);
-            newActive.classList.remove(CLASS_NAME_MOVE_RIGHT);
-        
-        } else {
+        this.hideSlide(this.active, direction);
+        this.showSlide(newActive, direction);
 
-            newActive.classList.add(CLASS_NAME_MOVE_LEFT);
-            this._reflow(newActive);
-
-            this.active.classList.add(CLASS_NAME_TRANSITION);
-            newActive.classList.add(CLASS_NAME_TRANSITION);
-
-            this.active.classList.add(CLASS_NAME_MOVE_RIGHT);
-            newActive.classList.remove(CLASS_NAME_MOVE_LEFT);
-
-        }
         this.active.classList.remove(CLASS_NAME_ACTIVE);
         newActive.classList.add(CLASS_NAME_ACTIVE);
 
@@ -101,10 +90,24 @@ class Slider {
 
         let self = this;
         this.images.ontransitionend = function(event) {
-            event.target.classList.remove(CLASS_NAME_TRANSITION, CLASS_NAME_MOVE_LEFT, CLASS_NAME_MOVE_RIGHT);
-
+            event.target.classList.remove(CLASS_NAME_TRANSITION, CLASS_NAME_HIDE_LEFT, CLASS_NAME_HIDE_RIGHT);
             self.resetTimeout();
         }
+    }
+
+    prepareNextSlide(slide, direction) {
+        //hide slide left or right (depending on the direction),
+        //to set animation's starting position
+        slide.classList.add(direction == CLASS_NAME_NEXT ? CLASS_NAME_HIDE_RIGHT : CLASS_NAME_HIDE_LEFT);
+        this._reflow(slide);
+    }
+
+    hideSlide(element, direction) {
+        element.classList.add(direction == CLASS_NAME_NEXT ? CLASS_NAME_HIDE_LEFT : CLASS_NAME_HIDE_RIGHT);
+    }
+
+    showSlide(element, direction) {
+        element.classList.remove(direction == CLASS_NAME_NEXT ? CLASS_NAME_HIDE_RIGHT : CLASS_NAME_HIDE_LEFT);
     }
 
     resetTimeout() {
@@ -113,10 +116,12 @@ class Slider {
     
     init() {
         clearTimeout(this.timeoutId);
-        // this.timeoutId = setTimeout(this.autoPlay.bind(this), this.timeoutInterval);
+        this.timeoutId = setTimeout(this.autoPlay.bind(this), this.timeoutInterval);
         this.active = document.querySelector(SELECTOR_ACTIVE);
         this.activeBreadcrumb = document.querySelector(SELECTOR_ACTIVE_BC);
     }
+
+    //Private util methods
 
     _getElementIndex(parentNode, element) {
         return Array.prototype.indexOf.call(parentNode.children, element);
